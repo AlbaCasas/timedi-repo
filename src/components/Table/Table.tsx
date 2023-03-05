43ad0d23e5529088fb80f68bff743f0c41d7426b
Alba Casas <albacasasarzua1b@gmail.com>
2023-03-05 20:26:35 +0100

    chore: prettier

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
-import type { SyntheticEvent , PropsWithChildren } from "react";
+import type { SyntheticEvent, PropsWithChildren } from "react";
 import RCTable from "rc-table";
 
 import { cls } from "@core/styles/cls";
